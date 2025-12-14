1fb21941e250f862d9a29cce46480c02d628bf66
23120396 <quangtuanxml@gmail.com>
2025-12-14 22:12:04 +0700

    Done Nav and create Footer

--- btcn02/src/components/webComponents/Nav.jsx
+++ btcn02/src/components/webComponents/Nav.jsx
@@ -13,28 +13,28 @@ export default function Nav() {
     }
   };
   return (
-    <nav className="bg-blue-300 dark:bg-gray-900 text-white p-3">
-      <div className="max-w-7xl mx-auto flex items-center gap-6">
+    <nav className="bg-navbg text-white p-3 rounded-sm">
+      <div className="mx-auto flex items-center gap-6">
         <Button
-          variant="ghost"
+          variant="outline"
           size="sm"
           onClick={() => {
             setSearchQuery("");
           }}
-          className="text-white hover:bg-blue-800 dark:hover:bg-gray-800"
+          className= "bg-navbg border-none hover:bg-transparent"
         >
           <House size={44} color="#000000" strokeWidth={2.75} />
         </Button>
-        <div className="flex-1 max-w-md flex gap-2">
+        <div className="flex-1 max-w-sm flex gap-2 ml-auto">
           <Input
             type="text"
             placeholder="Search"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
             onKeyPress={handleKeyPress}
-            className="bg-white dark:bg-gray-800 text-gray-900 dark:text-white"
+            className="bg-background text-foreground"
           />
-          <Button onClick={handleSearch} size="sm" className="">
+          <Button onClick={handleSearch} size="sm" className="bg-transparent text-green-800 border-2 border-green-800">
             Search
           </Button>
         </div>
